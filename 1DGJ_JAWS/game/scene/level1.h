abac8a714fe8a976482b1fc6573d04a721a7e8f2
takawo0946 <takawo0946@mail.com>
2025-04-27 10:56:05 +0900

    Level1にCamera追加

--- 1DGJ_JAWS/game/scene/level1.h
+++ 1DGJ_JAWS/game/scene/level1.h
@@ -12,10 +12,14 @@ class Level1 : public SceneManager<SceneEnum>::Scene {
 public:
 
   Level1(const InitData& init) : IScene { init } {
-    m_object_.Add(std::make_unique<game::Player>(Vec2(100, 100)));
+    player_ = m_object_.Add(std::make_unique<game::Player>(Vec2(100, 100)));
   }
 
   void update() override {
+    Optional<game::Player*> player = m_object_.GetAs<game::Player>(player_);
+    //camera_.setTargetCenter(Vec2(player.value()->GetPosition().x, 480));
+    //camera_.setTargetCenter(Vec2 (m_object_.GetAs<Player>(player_).value().x, 480));
+
     m_object_.Update();
 
     camera_.update();
@@ -28,7 +32,7 @@ public:
     Print << Profiler::FPS() << U" FPS\n";
   }
 
-
+  base::ObjectHandle player_;
   base::GameObjectManager m_object_;
   Camera2D camera_ { Vec2{ 0, 0 }, 1.0, CameraControl::None_ };
 };
