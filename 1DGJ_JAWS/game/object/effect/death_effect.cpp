acccc26ff59c9d728244d3fa4f55b40a85601b9f
chalp <chalp@AKKI>
2025-04-27 23:01:52 +0900

    デスエフェクトとりあえず追加

---
+++ 1DGJ_JAWS/game/object/effect/death_effect.cpp
@@ -0,0 +1,34 @@
+﻿#include "stdafx.h"
+#include "death_effect.h"
+
+namespace game {
+
+DeathEffect::DeathEffect(const Vec2& pos) {
+  position_ = pos;
+}
+
+bool DeathEffect::Update() {
+  position_ += Vec2(3, 0);
+  if (effect_frame_ > 2.0) {
+    // 消滅
+    return false;
+  } else {
+    effect_frame_ += Scene::DeltaTime();
+    visible = int(effect_frame_ * 10) % 2 == 0;
+    return true;
+  }
+}
+
+void DeathEffect::Render(const Camera2D& camera) const {
+  const auto t = camera.createTransformer();
+  if (visible) {
+    Circle(position_, effect_frame_ * 20).draw(Palette::Aqua);
+  }
+  else {
+    Circle(position_, effect_frame_ * 20).draw(Palette::White);
+  }
+
+}
+
+}
+
---
+++ 1DGJ_JAWS/game/object/effect/death_effect.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "base_system/game_object/game_object.h"
+
+namespace game {
+
+class DeathEffect : public base::GameObject {
+public:
+    DeathEffect(const Vec2& pos);
+  ~DeathEffect() override = default;
+
+  bool Update() override;
+  void Setup() override {}
+  void Render(const Camera2D& camera) const override;
+  void Release() override {}
+
+  Optional<int> UpdateOrder() const override { return 0; }
+  Optional<int> RenderOrder() const override { return 10; }
+  Optional<int> Tag() const override { return 1; }
+
+private:
+  Vec2 position_ {};
+  double effect_frame_;
+  bool visible { true };
+};
+} // namespace game
