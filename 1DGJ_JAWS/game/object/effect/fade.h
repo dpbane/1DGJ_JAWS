806281be90f842f5011c0521fc3da86e6ca5ff55
takawo0946 <takawo0946@mail.com>
2025-04-27 22:23:56 +0900

    手動フェード追加

---
+++ 1DGJ_JAWS/game/object/effect/fade.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "base_system/game_object/game_object.h"
+
+namespace game {
+
+class Fade : public base::GameObject {
+public:
+  Fade(const Rect& rect);
+  ~Fade() override = default;
+
+  bool Update() override;
+  void Setup() override {}
+  void Render(const Camera2D& camera) const override;
+  void Release() override {}
+
+  Optional<int> UpdateOrder() const override { return 20000; }  // 要調整
+  Optional<int> RenderOrder() const override { return 20000; } // 要調整
+  Optional<int> Tag() const override { return 100; }
+
+private:
+  Rect rect_;
+  double alpha_;
+};
+
+}
---
+++ 1DGJ_JAWS/game/object/effect/fede.cpp
@@ -0,0 +1,32 @@
+﻿#include "stdafx.h"
+#include "fade.h"
+
+namespace game {
+
+Fade::Fade(const Rect& rect) {
+  rect_ = rect;
+  alpha_ = 0.0;
+}
+
+bool Fade::Update() {
+  // 透明度を変化させる
+  alpha_ += Scene::DeltaTime();
+
+  if (alpha_ < 1) {
+    return true;
+  }
+  else {
+    rect_.draw(ColorF { 0, 0, 0, 1 });
+    return true;
+  }
+}
+
+void Fade::Render(const Camera2D& camera) const {
+  {
+    //const auto t = camera.createTransformer();
+    rect_.draw(ColorF { 0, 0, 0, alpha_ });
+  }
+}
+
+}
+
