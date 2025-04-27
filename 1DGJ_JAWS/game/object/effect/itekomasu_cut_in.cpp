7520c3529b0afd3d01f86d28c80d686c01580a42
takawo0946 <takawo0946@mail.com>
2025-04-27 14:20:22 +0900

    いてこますエフェクト作成(Camera参照しない版)

---
+++ 1DGJ_JAWS/game/object/effect/itekomasu_cut_in.cpp
@@ -0,0 +1,43 @@
+﻿#include "stdafx.h"
+#include "itekomasu_cut_in.h"
+
+#include "game/reference.h"
+
+namespace game {
+
+ItekomasuCutIn::ItekomasuCutIn(const Vec2& position) {
+  base_position_ = position;
+  effect_frame_ = 0;
+  texture_ = ref::MGraphic.GetTexture(graphic::Handle::Itekoma);
+}
+
+void ItekomasuCutIn::Setup() {
+}
+bool ItekomasuCutIn::Update() {
+  // positionをがたがた揺らす
+  // 揺れ幅は要調整
+  position_ = Vec2 { (base_position_.x + Random(1,80)), (base_position_.y + Random(1,50)) };
+
+  if (effect_frame_ > 0.5) {
+    // 消滅
+    return false;
+  }
+  else {
+    effect_frame_ += Scene::DeltaTime();
+    return true;
+  }
+}
+void ItekomasuCutIn::Render(const Camera2D& camera) const {
+  // const auto t = camera.createTransformer();
+  //texture_.scaled(0.8).draw(); // サイズ感は要調整
+  // なんかずれるのでcamera参照しないでもよいかも
+  texture_.scaled(0.8).drawAt(position_); // サイズ感は要調整
+
+  //texture_.drawAt(position_); // サイズ感は要調整
+
+}
+void ItekomasuCutIn::Release() {
+
+}
+
+}
