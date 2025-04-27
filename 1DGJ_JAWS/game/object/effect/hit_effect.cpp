00060583229ee3859a5b4407892cf4fb121eef6f
takawo0946 <takawo0946@mail.com>
2025-04-27 12:04:56 +0900

    ヒットエフェクトcppわけ

---
+++ 1DGJ_JAWS/game/object/effect/hit_effect.cpp
@@ -0,0 +1,48 @@
+﻿#include "stdafx.h"
+#include "hit_effect.h"
+
+namespace game {
+
+HitEffect::HitEffect(const Vec2& pos, double baseHue)
+{
+  for (int32 i = 0; i < 5; ++i)
+  {
+    const Vec2 velocity = RandomVec2(Circle { 60 });
+    Star star {
+      .start = (pos + velocity * 0.5),
+      .velocity = velocity,
+      .color = HSV{ baseHue + Random(-60.0, 20.0) },
+    };
+    m_stars << star;
+  }
+
+  effect_frame_ = 0;
+}
+
+bool HitEffect::Update()
+{
+  if (effect_frame_ > 0.2) {
+    // 消滅
+    return false;
+  }
+  else {
+    effect_frame_ += Scene::DeltaTime();
+    return true;
+  }
+}
+
+void HitEffect::Render(const Camera2D& camera) const {
+  for (auto& star : m_stars)
+  {
+    const Vec2 pos = (star.start + star.velocity * effect_frame_ * 5);
+    const double angle = (pos.x * 3_deg);
+
+    // サイズはいい感じに調整する
+    // ほんとはRenderに分けたほうが良い?
+    Shape2D::Star((16 * (1.0 - effect_frame_ * 5)), pos, angle).draw(star.color);
+  }
+
+}
+
+}
+
--- 1DGJ_JAWS/game/object/effect/hit_effect.h
+++ 1DGJ_JAWS/game/object/effect/hit_effect.h
@@ -6,9 +6,6 @@ namespace game {
 
 class HitEffect : public base::GameObject {
 public:
-
-  static constexpr Vec2 Gravity { 0, 0 };
-
   struct Star
   {
     Vec2 start;
@@ -17,51 +14,16 @@ public:
   };
 
   Array<Star> m_stars;
-  HitEffect(const Vec2& pos, double baseHue)
-  {
-    for (int32 i = 0; i < 3; ++i)
-    {
-      const Vec2 velocity = RandomVec2(Circle { 60 });
-      Star star {
-        .start = (pos + velocity * 0.5),
-        .velocity = velocity,
-        .color = HSV{ baseHue + Random(-60.0, 20.0) },
-      };
-      m_stars << star;
-    }
-
-    effect_frame_ = 0;
-  }
-
+  HitEffect(const Vec2& pos, double baseHue);
   ~HitEffect() override = default;
 
-  bool Update() override
-  {
-    for (auto& star : m_stars)
-    {
-      const Vec2 pos = (star.start + star.velocity * effect_frame_ * 5);
-      const double angle = (pos.x * 3_deg);
-
-      // サイズはいい感じに調整する
-      Shape2D::Star((32 * (1.0 - effect_frame_ * 5)), pos, angle).draw(star.color);
-    }
-
-    if (effect_frame_ > 0.2) {
-      // 消滅
-      return false;
-    }
-    else {
-      effect_frame_ += Scene::DeltaTime();
-      return true;
-    }
-  }
-
+  bool Update() override;
   void Setup() override {}
-  void Render(const Camera2D& camera) const override {}
+  void Render(const Camera2D& camera) const override;
   void Release() override {}
 
-  Optional<int> UpdateOrder() const override { return 0; }
-  Optional<int> RenderOrder() const override { return 10; }
+  Optional<int> UpdateOrder() const override { return 0; }  // 要調整
+  Optional<int> RenderOrder() const override { return 10; } // 要調整
   Optional<int> Tag() const override { return 1; }
 
 private:
